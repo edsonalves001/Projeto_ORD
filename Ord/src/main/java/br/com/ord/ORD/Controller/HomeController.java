b67a3173868284d1b679570f56f4e88fbed7113d
Igunho <igorsantos.7262@gmail.com>
2026-05-25 03:07:39 -0300

    Implementaçãomoni home
    monitor da home agora contem todos os topicos no banco e todos o niveis pertecentes aquele topico
    niveis ainda não poss todas as atividades pertecentele

--- Ord/src/main/java/br/com/ord/ORD/Controller/HomeController.java
+++ Ord/src/main/java/br/com/ord/ORD/Controller/HomeController.java
@@ -1,5 +1,8 @@
 package br.com.ord.ORD.Controller;
 
+import br.com.ord.ORD.model.Atividade;
+import br.com.ord.ORD.model.Topico;
+import br.com.ord.ORD.repository.AtividadeRepository;
 import br.com.ord.ORD.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,11 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class HomeController {
 
     @Autowired
     TopicoRepository topicoRepository;
+
+    @Autowired
+    AtividadeRepository atividadeRepository;
 
     @GetMapping("/home")
     public String OrdHome(Model model) {
@@ -21,9 +31,14 @@ public class HomeController {
         return "home";
     }
 
-    @PostMapping("/home/topico")
+    @PostMapping("/home")
     public String OrdAtividades(@RequestParam String topico, Model model) {
-        model.addAttribute("topico", topicoRepository.findById(topico));
+        Optional<Topico> topicoAtual = topicoRepository.findById(topico);
+        model.addAttribute("topicoNome", topicoAtual.map(Topico::getNome).orElse("nenhum"));
+        model.addAttribute("niveis", topicoAtual.map(Topico::getNiveis).orElse(null));
+        model.addAttribute("topicos", topicoRepository.findAll());
+        List<Atividade> atividades = atividadeRepository.findAll();
+        model.addAttribute("atividades",atividades);
 
         return "home";
     }
